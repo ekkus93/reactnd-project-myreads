ded6c9df3cf0b8a07b77c3180a159abae3458a30
Phillip Chin <ekkus93@gmail.com>
2020-02-02 12:14:22 -0800

    refactor: Split out search books bar into its own component.

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import BooksGrid from './BooksGrid'
+import SearchBooksBar from './SearchBooksBar'
+import SearchBookResults from './SearchBooksResults'
 import '../App.css'
 
 class SearchPage extends Component {
@@ -8,24 +9,13 @@ class SearchPage extends Component {
 
     return (
       <div className="search-books">
-        <div className="search-books-bar">
-          <button className="close-search" onClick={handleCloseSearchPage}>Close</button>
-          <div className="search-books-input-wrapper">
-            {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-            <input type="text" placeholder="Search by title or author" value={searchStr} onChange={handleSearchBooks}/>
-
-          </div>
-        </div>
-        <div className="search-books-results">
-          <BooksGrid books={books} handleBookChange={handleBookChange}/>
-        </div>
+        <SearchBooksBar
+          searchStr={searchStr}
+          handleCloseSearchPage={handleCloseSearchPage}
+          handleSearchBooks={handleSearchBooks} />
+        <SearchBookResults
+          books={books}
+          handleBookChange={handleBookChange}/>
       </div>
     );
   }
