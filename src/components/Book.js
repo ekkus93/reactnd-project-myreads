3de59677af25dcf3b1f45ad47b54ae643aff458b
Phillip Chin <ekkus93@gmail.com>
2020-02-02 02:31:14 -0800

    refactor: Updated props for Book to match api

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,19 +4,28 @@ import '../App.css'
 
 class Book extends Component {
     render() {
-        const { backgroundImage, width, height, title, author } = this.props;
+        console.log("###Book");
+        const { title, authors } = this.props;
+        const author = authors ? authors.join(", ") : "";
+        const backgroundImage = this.props.imageLinks.thumbnail;
         const backgroundImageVal = 'url("' + backgroundImage + '")';
         const bookCoverStyle = {
+            /*
             width: width,
             height: height,
+            */
+            width: 128,
+            height: 193,
             backgroundImage: backgroundImageVal,
+            'background-repeat': 'no-repeat',
+            'background-position': 'center bottom',
         }
 
         return (
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover" style={bookCoverStyle}></div>
-                    <BookshelfChanger/>
+                    <BookshelfChanger />
                 </div>
                 <div className="book-title">{title}</div>
                 <div className="book-authors">{author}</div>
--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -4,7 +4,7 @@ import '../App.css'
 
 class Bookshelf extends Component {
     render() {
-        console.log("###Bookshelf");
+        console.log("###Bookshelf1");
         const { title, books } = this.props;
 
         return (
@@ -12,7 +12,15 @@ class Bookshelf extends Component {
                 <h2 className="bookshelf-title">{title}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
-                        { books.map( book => (<Book {...book}/>) ) }
+                        { books.map( (book) => {
+                            const key = (!book.id) ? book.title : book.id;
+                            return (
+                                <li key={key}>
+                                    <Book {...book}/>
+                                </li>
+                                );
+                            })
+                        }
                     </ol>
                 </div>
             </div>
