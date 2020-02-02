d4e6dc01b214774d9d3769b7b35e40ed90377657
Phillip Chin <ekkus93@gmail.com>
2020-02-02 12:00:00 -0800

    fix: Split out SearchBookResults to reduce lagginess with search input.

---
+++ src/components/SearchBooksResults.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import BooksGrid from './BooksGrid'
+import '../App.css'
+
+class SearchBookResults extends Component {
+    render() {
+        const {books, handleBookChange} = this.props;
+
+        return (
+            <div className="search-books-results">
+                <BooksGrid books={books} handleBookChange={handleBookChange}/>
+          </div>
+        );
+    }
+}
+
+export default SearchBookResults;
+
